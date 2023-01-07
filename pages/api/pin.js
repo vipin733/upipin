3e760d660005cfcdc124c2d68235d420c2aa2da6
Vipin Kumar <vipinkumar021193@gmail.com>
2023-01-07 18:57:57 +0530

    added encryption

--- pages/api/pin.js
+++ pages/api/pin.js
@@ -1,7 +1,10 @@
 import moment from "moment"
 import { decryptPin, _csrfTokenDecode } from "../../lib/hashing"
+import { EncryptWithAES } from "../../lib/encryption"
+import axios from "axios"
 
-export default function handler(req, res) {
+
+export default async function handler(req, res) {
   try {
     let token = _csrfTokenDecode(req.body._csrf)
     let startTime = moment(token.time)
@@ -10,13 +13,28 @@ export default function handler(req, res) {
     let pin = decryptPin(req.body.pin)
 
     if (duration.asMinutes() > 5 || token.domain != req.headers.host) {
-      return res.status(500).json({message:"server error"})
+      return res.status(500).json({ message: "server error" })
     }
 
-    setTimeout(() => {
-      res.status(200).json({token, pin, duration:  duration.asMinutes(), startTime, host: req.headers.host, requestBody:req.body })
-    }, 5000);
+    pin = EncryptWithAES(pin)
+
+    let data = {
+      end: false,
+      data: {
+        mobile: "9164186611",
+        // pin: "RAeA5uuet8jiKHixZ5UWwKL2K8uO2M1FZQst5M+GzTA=", 
+        pin, 
+        merchant_id: "0000104427", 
+        amount: "1"
+      }
+    }
+    console.log(data);
+
+    let response = await axios.post(process.env.URL, data)
+    res.status(200).json({...response.data })
+
   } catch (error) {
-    return res.status(500).json({message:"server error"})
+    return res.status(500).json({ message: "server error" , error: error.response.data})
   }
 }
+
