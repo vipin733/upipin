6f3ec218f07d5dd40926979bf81b1b3da5feb0ea
Vipin Kumar <vipinkumar021193@gmail.com>
2023-01-08 15:47:46 +0530

    removed try catch

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -2,7 +2,7 @@ import { _createPaymentUrl } from "../../lib/generateUrl"
 import { getCurrentBaseUrl } from "../../lib/helper";
 import url from "url"
 export default async function handler(req, res) {
-  try {
+  // try {
     let {amount, mobile, merchant_id, bank, merchant_name} = req.body
     if (!amount || !mobile || !merchant_id || !bank || !merchant_name) {
       return res.status(400).json({ message: "request not valid"})
@@ -11,8 +11,8 @@ export default async function handler(req, res) {
     let urldata = new url.URL(getCurrentBaseUrl(req).__NEXT_INIT_URL)
     let paymentUrl = `${urldata.origin}/${encryption}`
     res.status(200).json({status: true,  payment_url: paymentUrl})
-  } catch (error) {
-    return res.status(500).json({ message: "server error" , error})
-  }
+  // } catch (error) {
+  //   return res.status(500).json({ message: "server error" , error})
+  // }
 }
 
