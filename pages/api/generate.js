8923d9b162545c1954296e69c09207b17c4929ab
Vipin Kumar <vipinkumar021193@gmail.com>
2023-01-08 16:01:04 +0530

    added base url

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,19 +1,15 @@
 import { _createPaymentUrl } from "../../lib/generateUrl"
-// import { getCurrentBaseUrl } from "../../lib/helper";
-// import url from "url"
 export default async function handler(req, res) {
-  // try {
+  try {
     let {amount, mobile, merchant_id, bank, merchant_name} = req.body
     if (!amount || !mobile || !merchant_id || !bank || !merchant_name) {
       return res.status(400).json({ message: "request not valid"})
     }
     let encryption = await _createPaymentUrl({amount, mobile, merchant_id, bank, merchant_name})
-    // let urldata = new url.URL(getCurrentBaseUrl(req).__NEXT_INIT_URL)
-    // let paymentUrl = `${urldata.origin}/${encryption}`
-    let paymentUrl = encryption
+    let paymentUrl = `${process.env.BASE_URL}/${encryption}`
     res.status(200).json({status: true,  payment_url: paymentUrl})
-  // } catch (error) {
-  //   return res.status(500).json({ message: "server error" , error})
-  // }
+  } catch (error) {
+    return res.status(500).json({ message: "server error" , error})
+  }
 }
 
